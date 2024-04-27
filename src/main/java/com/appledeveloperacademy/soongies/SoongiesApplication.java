c266ee1a88159238fce35fa71a24dcc69f52db6e
KyungsooLee <jmtkd9196@gmail.com>
2024-04-27 20:01:35 +0900

    Init Soongies spring project

---
+++ src/main/java/com/appledeveloperacademy/soongies/SoongiesApplication.java
@@ -0,0 +1,13 @@
+package com.appledeveloperacademy.soongies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoongiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoongiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appledeveloperacademy/soongies/SoongiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appledeveloperacademy.soongies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoongiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
