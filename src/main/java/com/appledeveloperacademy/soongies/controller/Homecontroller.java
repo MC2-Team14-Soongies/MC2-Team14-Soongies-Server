e1e84645d05ec8cd3bb053c71636f52fe7e9ff14
KyungsooLee <jmtkd9196@gmail.com>
2024-04-27 20:11:29 +0900

    Setting Soongies project

---
+++ src/main/java/com/appledeveloperacademy/soongies/controller/Homecontroller.java
@@ -0,0 +1,22 @@
+package com.appledeveloperacademy.soongies.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homecontroller {
+
+    @Value("${app.version:1.0.0}")
+    String version;
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello CI/CD! version: " + version;
+    }
+
+    @GetMapping("/liver")
+    public String getNickname() {
+        return "My name is liver";
+    }
+}
