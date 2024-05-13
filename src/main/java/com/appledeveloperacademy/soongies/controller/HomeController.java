63c9f4767be65b400524a3530cbab5f323efddec
KyungsooLee <jmtkd9196@gmail.com>
2024-05-13 09:29:05 +0900

    chore: Setting swagger config

--- src/main/java/com/appledeveloperacademy/soongies/controller/HomeController.java
+++ src/main/java/com/appledeveloperacademy/soongies/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.appledeveloperacademy.soongies.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "swagger setting test", description = "스웨거 테스트")
 @RestController
-public class Homecontroller {
+@RequiredArgsConstructor
+public class HomeController {
 
     @Value("${app.version:1.0.0}")
     String version;
@@ -17,16 +21,19 @@ public class Homecontroller {
         return "Hello CI/CD! version: " + version;
     }
 
+    @Operation(summary = "리버 테스트 API", description = "CI/CD 및 스웨거 확인용 API입니다.")
     @GetMapping("/liver")
     public String getLiverNickname() {
         return "My name is Liver";
     }
 
+    @Operation(summary = "카야 출력 테스트 API", description = "CI/CD 및 스웨거 확인용 API입니다.")
     @GetMapping("/kaya")
     public String getKayaNickname() {
         return "My name is Kaya";
     }
 
+    @Operation(summary = "닉네임 입력/출력 테스트 API", description = "CI/CD 및 스웨거 확인용 API입니다.")
     @GetMapping("/getNickname")
     public String getNickname(
             @RequestParam(name = "nickname") String nickname
